a7559a21e89f6a2b06bcaf532e38b81dcd046dcc
herding-wolf <herding.wolf.lc@gmail.com>
2020-07-20 18:06:06 +0800

    动态组件

---
+++ src/TestCom.js
@@ -0,0 +1,46 @@
+/*
+ * @Author: huangyuhui
+ * @since: 2020-07-20 08:48:33
+ * @LastAuthor: huangyuhui
+ * @lastTime: 2020-07-20 18:01:50
+ * @message: 
+ * @FilePath: \supply-chain-system\src\TestCom.js
+ */
+const cache = {};
+async function getComponent ( component, obj, name ) {
+  try {
+    const res = await component();
+    if ( '__esModule' in res ) {
+      obj[ name ] = res.default;
+      this.$forceUpdate();
+    }
+
+  } catch ( error ) {
+    console.log( error );
+  }
+}
+export default {
+  functional: true,
+  props: {
+    cname: {
+      type: String,
+      default: ''
+    }
+  },
+  render ( _, { props, parent, children } ) {
+    const { cname, ...o } = props;
+    const currentCom = parent.$options.components[ props.cname ];
+    const h = parent.$createElement;
+    if ( props.cname in cache ) {
+      const com = cache[ props.cname ];
+      return h( com, {
+        props: {
+          list: []
+        }
+      }, children );
+    } else {
+      getComponent.call( parent, currentCom, cache, props.cname );
+      return h( 'div', '' );
+    }
+  }
+};
