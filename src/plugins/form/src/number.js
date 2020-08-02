faaf925fb9c3ccd503d10dec73a1f7a5d4841a1c
huangyuhui <310515005@qq.com>
2020-08-02 19:19:03 +0800

    添加校验数字

--- src/plugins/form/src/number.js
+++ src/plugins/form/src/number.js
@@ -1,12 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-07-31 15:34:56
- * @LastEditTime: 2020-08-02 17:59:40
+ * @LastEditTime: 2020-08-02 19:18:47
  * @LastEditors: Please set LastEditors
  * @Description: 数字输入框
  * @FilePath: /supply-chian-system/src/plugins/form/src/number.js
- */ 
+ */
 import { FormItem, Input } from 'element-ui';
+import { debounce } from 'lodash';
 export default {
   abstract: true,
   props: {
@@ -28,21 +29,52 @@ export default {
     InputComponent: Input
   },
   render ( h ) {
-    const { prop, disabled, readonly, rules = [], label = '', placeholder } = this.currentConf;
+    const { prop, disabled, readonly, rules = [], label = '', placeholder, decimal = 0 } = this.currentConf;
     return h(
       'FormItem',
       {
         props: {
           label,
           prop,
-          rules
+          rules: [
+            ...rules,
+            {
+              validator:
+                ( _, value, callback ) => {
+                  value = String( value );
+
+                  /* 校验是否位数字 */
+                  const reg = new RegExp( `^\\d+(\\.\\d{0,${decimal}})?$` );
+                  let isNumber = reg.test( value );
+                  console.log( isNumber );
+
+                  /* 小数长度 */
+                  const decimalLen = ( value.split( /\./ )[ 1 ] ?? '' ).length;
+
+                  /* 提示信息 */
+                  let msg = void 1;
+                  if ( decimal === 0 && decimalLen > decimal ) {
+                    msg = '请输入整数';
+                  } else if ( decimalLen > decimal ) {
+                    msg = `超过${decimal}位小数`;
+                  } else if ( rules.length === 0 ) {
+                    msg = void 1;
+                  } else if ( isNumber === false ) {
+                    msg = '请输入正确数值';
+                  }
+                  callback(
+                    msg
+                  );
+                }
+            }
+          ]
         }
       },
       [
         h(
           'InputComponent',
           {
-            attrs:{
+            attrs: {
               id: prop,
               placeholder
             },
